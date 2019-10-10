ca3aaeefd6c01ce4bc0c38f647310816dc70c675
Felipe Homrich Melchior <fehmel@gmail.com>
2019-10-09 21:28:33 -0300

    feat: add routes

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,21 +1,34 @@
+import 'dotenv/config';
 import express from 'express';
-import Youtch from 'youtch';
+import Youch from 'youch';
+
+import routes from './Routes';
 
 class App {
   constructor() {
     this.server = express();
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
     this.server.use(express.json());
   }
 
-  routes() {}
+  routes() {
+    this.server.use(routes);
+  }
 
   exceptionHandler() {
-    // Youch
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
+      return res.status(500).json({ error: 'Internal Server Error' });
+    });
   }
 }
 
