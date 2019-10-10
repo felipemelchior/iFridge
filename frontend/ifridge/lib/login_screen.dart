d96026ec36eca04606249450bdcb3c9073dbbbef
Rafael Fernandes <faelsfernandes@gmail.com>
2019-10-09 23:49:46 -0300

    Add: Botões da tela de cadastro

--- frontend/ifridge/lib/login_screen.dart
+++ frontend/ifridge/lib/login_screen.dart
@@ -7,117 +7,235 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final Shader linearGradient = LinearGradient(
-  colors: <Color>[Colors.yellow, Colors.red],
-).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
-
+    colors: <Color>[Colors.pink, Colors.blueGrey],
+  ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.only(
-          top: 60,
-          left: 40,
-          right: 40,
-        ),
-        color: Colors.white,
-        child: ListView(
-          children: <Widget>[
-            SizedBox(
-              height: 150,
-              width: 130,
-              child: Container(
-                alignment: Alignment.center,
-                child: Text(
-                      "iFridge!",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 50,
-                        fontFamily: "Merriweather",
-                        foreground: Paint()..shader = linearGradient,
-                        shadows: [
-                          Shadow(
-                            blurRadius: 2,
-                            color: Colors.grey,
-                            offset: Offset(3, 3)
+          color: Colors.white,
+          child:Column(
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  // height: MediaQuery.of(context).size.height/3,
+                  height: 180,
+                  // decoration: BoxDecoration(
+                  //   gradient: LinearGradient(
+                  //     colors: [
+                  //       Color(0xFFf45d27),
+                  //       Color(0xFFf5851f),
+                  //     ],
+                  //   ),
+                  //   borderRadius: BorderRadius.only(
+                  //     bottomLeft: Radius.circular(50),
+                  //     bottomRight: Radius.circular(50),
+                  //   ),
+                  // ),
+                  child: CustomPaint(
+                    painter: ShapeCurvo(),
+                    child: SizedBox(
+                      height: 100,
+                      width: 130,
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "iFridge",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 50,
+                            fontFamily: "Merriweather",
+                            foreground: Paint()..shader = linearGradient,
+                            shadows: [
+                              Shadow(
+                                  blurRadius: 2,
+                                  color: Colors.black,
+                                  offset: Offset(3, 3)),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  padding: EdgeInsets.only(
+                    top: 0,
+                    left: 40,
+                    right: 40,
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.only(
+                          bottom: 30,
+                        ),
+                        alignment: Alignment.center,
+                        child: RaisedButton(
+                          color: Colors.grey,
+                          child: Text(
+                            "Logar com Facebook",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              fontFamily: "Merriweather",
+                              color: Colors.black,
+                              // shadows: [
+                              //   Shadow(
+                              //       blurRadius: 2,
+                              //       color: Colors.black,
+                              //       offset: Offset(3, 3)),
+                              // ],
+                            ),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Divider(),
+                          ),
+                          Text(
+                            "Ou",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 10,
+                              fontFamily: "Merriweather",
+                            ),
+                          ),
+                          Expanded(
+                            child: Divider(),
                           ),
                         ],
                       ),
-                    ),
-              ),
-            ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: "E-mail",
-                labelStyle: TextStyle(
-                  color: Colors.deepPurple,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 30,
-                ),
-              ),
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Senha",
-                labelStyle: TextStyle(
-                  color: Colors.deepPurple,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                ),
-              ),
-              style: TextStyle(
-                fontSize: 30,
-              ),
-            ),
-            Container(
-              height: 50,
-              alignment: Alignment.center,
-              child: RaisedButton(
-                      color: Colors.deepPurpleAccent,
-                      child: Text(
-                        "Entrar",
-                        textAlign: TextAlign.center,
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          // labelText: "E-mail",
+                          hintText: "E-mail",
+                          labelStyle: TextStyle(
+                            color: Colors.deepPurple,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 20,
+                          ),
+                        ),
                         style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
+                          fontSize: 10,
                         ),
                       ),
-                      shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(4)
+                      SizedBox(
+                        height: 20,
                       ),
-                      onPressed: (){},
-                    ),
-                    padding: EdgeInsets.only(top: 15),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 20,
-              alignment: Alignment.center,
-              child: FlatButton(
-                      child: Text(
-                        "Esqueci minha senha",
-                        textAlign: TextAlign.right,
+                      TextFormField(
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          // labelText: "Senha",
+                          hintText: "Senha",
+                          labelStyle: TextStyle(
+                            color: Colors.deepPurple,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 10,
+                          ),
+                        ),
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
                       ),
-                      onPressed: () {},
-                    ),
+                      Container(
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: RaisedButton(
+                          color: Colors.deepPurpleAccent,
+                          child: Text(
+                            "Entrar",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 10,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(4)),
+                          onPressed: () {},
+                        ),
+                        padding: EdgeInsets.only(top: 15),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        height: 20,
+                        alignment: Alignment.center,
+                        child: FlatButton(
+                          child: Text(
+                            "Esqueci minha senha",
+                            textAlign: TextAlign.right,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            child: Text("Ainda não é cadastrado?",
+                                    textAlign: TextAlign.center,
+                                  ),                                
+                          ),
+                          Container(
+                            // height: 20,
+                            alignment: Alignment.centerLeft,
+                            child: FlatButton(
+                              child: Text(
+                                "Clique aqui!",
+                                textAlign: TextAlign.center,
+                              ),
+                              padding: EdgeInsets.only(
+                                right: 50,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Divider(),
-
-          ],
-        ),
-      ),
+          ),
     );
+  }
+}
+
+class ShapeCurvo extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Colors.deepPurple;
+    paint.style = PaintingStyle.fill; // Change this to fill
+
+    var path = Path();
+
+    path.moveTo(0, size.height * 0.50);
+    path.quadraticBezierTo(
+        size.width, size.height, size.width / 1.5, size.height / 1.5);
+    // path.lineTo(size.width/2, size.height/2);
+    path.quadraticBezierTo(
+        size.width / 2.5, size.height / 3, size.width, size.height * 0.80);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
