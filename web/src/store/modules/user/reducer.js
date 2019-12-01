2f1df30dd02a5a4096096c87d9a965b4ad3d82ad
Felipe Homrich Melchior <fehmel@gmail.com>
2019-12-01 15:12:25 -0300

    feat: add user modules

--- web/src/store/modules/user/reducer.js
+++ web/src/store/modules/user/reducer.js
@@ -1,17 +1,14 @@
 import produce from 'immer';
 
 const INITIAL_STATE = {
-  token: null,
-  signed: false,
-  loading: false,
+  profile: null,
 };
 
-export default function auth(state = INITIAL_STATE, action) {
+export default function user(state = INITIAL_STATE, action) {
   switch (action.type) {
     case '@auth/SIGN_IN_SUCCESS':
       return produce(state, draft => {
-        draft.token = action.payload.token;
-        draft.signed = true;
+        draft.profile = action.payload.user;
       });
     default:
       return state;
