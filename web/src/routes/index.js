b751e6e66656fa5f6e931597b839bc7e477e8898
Felipe Homrich Melchior <fehmel@gmail.com>
2019-12-01 19:25:15 -0300

    feat: add product list

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -8,6 +8,9 @@ import SignUp from '~/pages/SignUp';
 import Dashboard from '~/pages/Dashboard';
 import Profile from '~/pages/Profile';
 
+import NewProduct from '~/pages/NewProduct';
+import NewType from '~/pages/NewType';
+
 export default function Routes() {
   return (
     <Switch>
@@ -16,6 +19,9 @@ export default function Routes() {
 
       <Route path="/dashboard" component={Dashboard} isPrivate />
       <Route path="/profile" component={Profile} isPrivate />
+
+      <Route path="/product" component={NewProduct} isPrivate />
+      <Route path="/type" component={NewType} isPrivate />
     </Switch>
   );
 }
