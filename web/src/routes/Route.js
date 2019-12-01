5633ac5b0eab23b3d86b919d7889c0c56438c733
Felipe Homrich Melchior <fehmel@gmail.com>
2019-12-01 20:11:23 -0300

    chore: shits happens ¯\_(ツ)_/¯

--- web/src/routes/Route.js
+++ web/src/routes/Route.js
@@ -1,20 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-=======
-import PropTypes from 'prop-types';
-import { Route, Redirect } from 'react-router-dom';
->>>>>>> b301c18eed188e88a872075c7fde9ae423bb7874
 
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
-<<<<<<< HEAD
 import { store } from '~/store';
 
-=======
->>>>>>> b301c18eed188e88a872075c7fde9ae423bb7874
 export default function RouteWrapper({
   component: Component,
   isPrivate,
