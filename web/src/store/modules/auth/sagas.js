b301c18eed188e88a872075c7fde9ae423bb7874
Felipe Homrich Melchior <fehmel@gmail.com>
2019-12-01 14:26:02 -0300

    feat: add api service

--- web/src/store/modules/auth/sagas.js
+++ web/src/store/modules/auth/sagas.js
@@ -1,3 +1,26 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+import history from '~/services/history';
+
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', { email, password });
+
+  const { token, user } = response.data;
+
+  if (!user.merchant) {
+    toast.error('Serviço disponível apenas para comerciantes');
+    return;
+  }
+
+  yield put(signInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
